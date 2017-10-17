1c808d492c633ed148302023adeb6fb4b0fa9597
nadiabn <bouna.nadia.isamm@gmail.com>
2017-10-17 12:25:12 +0200

    authentification google Api

---
+++ nadiabn/agenda/callback.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setAuthConfigFile('client_secret.json');
+$client->setRedirectUri('http://localhost/CDNL_nbouna/CDNL_17-18/nadiabn/agenda/callback.php');
+$client->addScope(array("https://www.googleapis.com/auth/calendar"));
+
+//print_r($_GET);
+
+if (! isset($_GET['code'])) {
+	$auth_url = $client->createAuthUrl();
+	header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+} else {
+	$client->authenticate($_GET['code']);
+	$_SESSION['access_token'] = $client->getAccessToken();
+	$redirect_uri = 'http://localhost/CDNL_nbouna/CDNL_17-18/nadiabn/agenda/index.php';
+	header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
