a42c25ed93967916f7b431c34b6cd8653df12475
geekloper <derfoufiabdel@gmail.com>
2017-10-23 17:03:42 +0200

    [Geekloper] [Modif] Architecture fichier

--- geekloper/agenda/callback.php
+++ geekloper/agenda/callback.php
@@ -1,21 +1,20 @@
 <?php
-require_once '../../../google-api-php-client-2.2.0/vendor/autoload.php';
+
+include 'lib/class.inc.php';
 
 session_start();
 
-$client = new Google_Client();
-$client->setAuthConfigFile('client_secret.json');
-$client->setRedirectUri("http://" . $_SERVER['HTTP_HOST'] . "/THYP_17-18/geekloper/agenda/callback.php");
-$client->addScope(array("https://www.googleapis.com/auth/calendar"));
+$client_secret_link = "client_secret.json";
+$RedirectUri = "http://" . $_SERVER['HTTP_HOST'] . "/THYP_17-18/geekloper/agenda/callback.php";
 
-//print_r($_GET);
+$google_agenda = new GoogleAgenda($client_secret_link,$RedirectUri);
 
 if (! isset($_GET['code'])) {
-	$auth_url = $client->createAuthUrl();
+	$auth_url = $google_agenda->client->createAuthUrl();
 	header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 } else {
-	$client->authenticate($_GET['code']);
-	$_SESSION['access_token'] = $client->getAccessToken();
+	$google_agenda->client->authenticate($_GET['code']);
+	$_SESSION['access_token'] = $google_agenda->client->getAccessToken();
 	$redirect_uri = "http://" . $_SERVER['HTTP_HOST'] . "/THYP_17-18/geekloper/agenda/index.php?q=all";
 	header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
