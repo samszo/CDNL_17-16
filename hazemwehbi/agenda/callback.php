f920251412e51d8a613f5dd0588c677ed186d34e
hazem wehbi <32312151+hazemwehbi@users.noreply.github.com>
2017-10-23 20:10:35 +0200

    Update callback.php

--- hazemwehbi/agenda/callback.php
+++ hazemwehbi/agenda/callback.php
@@ -5,7 +5,7 @@ session_start();
 
 $client = new Google_Client();
 $client->setAuthConfigFile('client_secret.json');
-$client->setRedirectUri('http://localhost/THYP_17-18/samszo/googleAuth/callback.php');
+$client->setRedirectUri('http://" . $_SERVER['HTTP_HOST'] . "/THYP_17-18/hazemwehbi/agenda/callback.php');
 //$client->addScope(array("https://www.googleapis.com/auth/calendar"));
 $client->setScopes ( array ('https://www.googleapis.com/auth/drive' ) );
 
@@ -15,6 +15,8 @@ if (! isset($_GET['code'])) {
 } else {
 	$client->authenticate($_GET['code']);
 	$_SESSION['access_token'] = $client->getAccessToken();
-	$redirect_uri = 'http://localhost/THYP_17-18/samszo/googleAuth/index.php';
+	$redirect_uri = "http://" . $_SERVER['HTTP_HOST'] . "/THYP_17-18/hazemwehbi/agenda/index.php?q=all";
 	header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
+
+?>
