8f738b38f38d6b45fa4acf3d5de4c5eadbaa8231
arezki roza <roza.arezki@outlook.fr>
2017-11-03 08:15:09 +0100

    ajoutpresent

--- rozaarezki/agenda/callbackCalender.php
+++ rozaarezki/agenda/callbackCalender.php
@@ -5,10 +5,9 @@ session_start();
 
 $client = new Google_Client();
 $client->setAuthConfigFile('client_secret.json');
-$client->setRedirectUri('http://localhost/THYP_17-18/rozaarezki/agenda/callback.php');
+$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/THYP_17-18/rozaarezki/agenda/callbackCalender.php');
 $client->addScope(array("https://www.googleapis.com/auth/calendar"));
-
-//print_r($_GET);
+//$client->setScopes ( array ('https://www.googleapis.com/auth/drive' ) );
 
 if (! isset($_GET['code'])) {
 	$auth_url = $client->createAuthUrl();
@@ -16,6 +15,7 @@ if (! isset($_GET['code'])) {
 } else {
 	$client->authenticate($_GET['code']);
 	$_SESSION['access_token'] = $client->getAccessToken();
-	$redirect_uri = 'http://localhost/THYP_17-18/rozaarezki/agenda/index.php?q=all';
+	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/THYP_17-18/rozaarezki/agenda/calender.php?q=all';
 	header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
+?>
