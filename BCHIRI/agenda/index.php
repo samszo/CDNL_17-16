ca37ff9b76ef71dbff988459a496568e17f2ebc3
ameni26 <amenibenrad@gmail.com>
2017-11-29 13:50:03 +0100

    calendar

--- BCHIRI/agenda/index.php
+++ BCHIRI/agenda/index.php
@@ -3,13 +3,11 @@ require_once '../../../google-api-php-client-2.2.0/vendor/autoload.php';
 
 session_start();
 
-
 $client = new Google_Client();
+
 $client->setAuthConfig('client_secret.json');
 //$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-//$client->addScope(array("https://www.googleapis.com/auth/drive"));
-$client->addScope(Google_Service_Drive::DRIVE);
-
+$client->addScope(array("https://www.googleapis.com/auth/calendar"));
 
 
 if(isset($_GET['out'])){
@@ -17,79 +15,63 @@ if(isset($_GET['out'])){
     $client->revokeToken();
 }
 
-
+//vérifie que le token n'ets pas expéré
+//if ($client->isAccessTokenExpired()) {
+//    unset($_SESSION['access_token']);
+//}
 
 
 //pour supprimer les droits https://myaccount.google.com/permissions?pli=1
 
 //print_r($_SESSION['access_token']);
-//$_SESSION['access_token'] = array("access_token"=>"ya29.GlvYBAAizcoG4SH14m1nTmBnZXqgabVmkNJyd0d1wFBMfDOTDmJvHWaD86CRJjFXRSY0SEiTfZjpvpWGzFAAkTfuhCICZ_hznkuCkDtSI5OIlCAz2M4aPOwZp3jS","token_type"=>"Bearer", "expires_in"=>"3599", "created"=>1506954203);
+//$_SESSION['access_token'] = array("access_token"=>"ya29.GlznBOwSIyspxzMQnUG7IVmqqUUnQ5c7GXY16rPPqPo6nrJ80rUK0WUQwootMzwuNPQLrTKUfITfN71XM-g0zii6yu_V6ugGE4Jsp56mV2bWH0UbsmUdV6-kyTnNMw","token_type"=>"Bearer", "expires_in"=>"3599", "created"=>1508238940);
 
 
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
 	$client->setAccessToken($_SESSION['access_token']);
-//	$cal_service = new Google_Service_Calendar($client);
+	$cal_service = new Google_Service_Calendar($client);
 	
-$service = new Google_Service_Drive( $client );
-
 	try {
-	    
+
 	    switch ($_GET['q']) {
 	        case 'all':
-	            // Lists the user's files.
-	            {	getallfiles($service);	break;}
-	        case 'app':
-	            // Lists a user's installed apps.
-	            {retrieveAllApps($service);break; }
-				
+	            //Pour la liste complète des calendrier de la personne
+	            $r = getAllCalendar($cal_service);
+        	        break;
+	        case 'info':
+	            //Pour les infos d'un calendrier
+	            $calendar = $cal_service->calendarList->get($_GET['id']);
+	            $r = getCalendarInfo($calendar, $cal_service);
+
+	            break;
+          case 'event':
+              //Pour les événements
+           getAllEvent($cal_service);
+                 break;
+	        case 'present':
+	            //Pour ajouter un présent
+	            $r = insertPresent($cal_service, $_GET['id'], $_GET['desc'], $_GET['email']);
+	            break;
+          
 	        default:
 	            $r = "rien";
 	           break;
 	    }
-	    	echo json_encode($r);    
+	    	echo json_encode($r);
 	} catch (Exception $e) {
 	    echo 'ERREUR : ',  $e->getMessage(), "\n";
 	}
 	//
 } else {
-$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/THYP_17-18/BCHIRI/agenda/callback.php';
+	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/THYP_17-18/BCHIRI/agenda/callback.php';
 	header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 
-// Lists the user's files.
-		function getallfiles($service)
-		{
-		$files_list = $service->files->listFiles(array());
-		if (count($files_list->getFiles()) == 0) {
-			print "No files found.\n";
-		} else {
-			foreach ($files_list->getFiles() as $file) {
-				$res['name'] = $file->getName();
-				$res['id'] = $file->getId();
-				$files[] = $res;
-			}
-			print_r($files);
-		}
 
-		}
-// Lists a user's installed apps.
-		function retrieveAllApps($service) {
-		  try {
-			$apps = $service->apps->listApps();
-			return $apps->getFiles();
-		  } catch (Exception $e) {
-			print "An error occurred: " . $e->getMessage();
-		  }
-		  return NULL;
-		}
-
-
-
-/*
 function getAllCalendar($service)
 {
     //Pour la liste complète des calendrier de la personne
-    $calendarList = 	$service->calendarList->listCalendarList();    
+    $calendarList = 	$service->calendarList->listCalendarList();
     while(true) {
         foreach ($calendarList->getItems() as $calendarListEntry) {
             $calendars[] = getCalendarInfo($calendarListEntry, $service);
@@ -103,25 +85,27 @@ function getAllCalendar($service)
         }
     }
     return $calendars;
-    
+
 }
-    
+
+
+
 function getCalendarInfo($cal, $service)
 {
-    
+
     $r = array("summary"=>$cal->getSummary()
         ,"id"=>$cal->getId()
         ,"access"=>$cal->getAccessRole()
         ,"description"=>$cal->getDescription()
         ,"location"=>$cal->getLocation()
     );
-        
+
     //récupère les roles
     if($r["access"]!="writer" && $r["access"]!="reader"){
         $roles = getListeAcl($r["id"], $service);
         $r["roles"]=$roles;
     }
-    
+
     return $r;
 }
 
@@ -130,7 +114,7 @@ function getListeAcl($idCal, $service)
     $acls ="";
     $acl = $service->acl->listAcl($idCal);
     foreach ($acl->getItems() as $rule) {
-        $acls[]=getAclInfo($rule);
+        $acls=getAclInfo($rule);
     }
     return $acls;
 }
@@ -144,29 +128,43 @@ function getAclInfo($acl)
     return $r;
 }
 
-function insertPresent($service, $calendarId){
-    
+function insertPresent($service, $calendarId, $desc, $mails,$dates){
+  $date = new DateTime();
+    //merci à https://developers.google.com/google-apps/calendar/v3/reference/events/insert
+    $dateDeb = $date->format('Y-m-d').'T'.$date->format('H:i:s');//'2017-10-17T14:30:00'
+    $date->add(new DateInterval('PT60S'));
+    $dateFin = $date->format('Y-m-d').'T'.$date->format('H:i:s');
+    echo $dateDeb." - ".$dateFin;
+    $attendees = array();
+    foreach ($mails as $m) {
+        $attendees[]=array('email'=>$m);
+    }
+    /*
+     * array(
+            array('email' => 'lpage@example.com'),
+            array('email' => 'sbrin@example.com'),
+        )
+     */
+    //pour la géolocalisation merci à https://stackoverflow.com/questions/409999/getting-the-location-from-an-ip-address
+
     $event = new Google_Service_Calendar_Event(array(
         'summary' => 'Présent',
         'location' => 'Paris 8',
-        'description' => 'Cours E-service',
+        'description' => $desc,
         'start' => array(
-            'dateTime' => '2017-10-02T09:00:00',
+            'dateTime' => $dateDeb,
             'timeZone' => 'Europe/Paris',
         ),
         'end' => array(
-            'dateTime' => '2017-10-02T10:00:00',
+            'dateTime' => $dateFin,
             'timeZone' => 'Europe/Paris',
         ),
-        'attendees' => array(
-            array('email' => 'lpage@example.com'),
-            array('email' => 'sbrin@example.com'),
-        ),
+        'attendees' => $attendees,
     ));
-    
+    //print_r($event);
     $event = $service->events->insert($calendarId, $event);
     return array('message'=>'Event created', 'event'=>$event);
-    
+
 }
-*/
+
 ?>
