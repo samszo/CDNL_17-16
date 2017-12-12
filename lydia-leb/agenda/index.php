01f02f89f98111c750e145189cc6d0735f385f51
lebaz lydia <lydia.lebaz@gmail.com>
2017-12-12 14:38:46 +0100

    version finale

--- lydia-leb/agenda/index.php
+++ lydia-leb/agenda/index.php
@@ -43,7 +43,7 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
         echo 'ERREUR : ',  $e->getMessage(), "\n";
     }
     //
-} else {
+} else {//s'il est nest pas connecté
     $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/THYP_17-18/lydia-leb/agenda/callback.php';
     header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
